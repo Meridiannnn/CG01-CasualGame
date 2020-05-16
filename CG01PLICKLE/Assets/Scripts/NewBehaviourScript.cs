454941330d96211384015c981410b15f1c51cdc2
Meridiannnn <54569336+Meridiannnn@users.noreply.github.com>
2020-05-16 16:15:55 -0700

    made crystal hit wall

---
+++ CG01PLICKLE/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // add a public variable force allowing us to have a variable force
+    public float force = 10;
+
+    // FixedUpdate is used to update for physics
+    void FixedUpdate()
+    {
+        // use rigidbodies and get the rigidbody component to add a force
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0, 0, force));
+    }
+}
